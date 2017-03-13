48c10f34ee153a2143518d0991cc409177afc5f1
Gabriel Velasco <gabriel.velasco001@gmail.com>
2017-03-13 15:27:02 -0400

    Finished adding tests for all the angular stuff, I'm sure that it doesn't work yet but yeah, testing works

--- app/client/src/main/routes.js
+++ app/client/src/main/routes.js
@@ -1,16 +1,20 @@
 'use strict';
 
-angular.module('main_routes', [])
-  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProveder
-      .when('/', {
-        templateUrl: absolute_path('/client/src/main/home.html'),
-        controller: 'main_controller'
-      })
-      .when('/sensors', {
-        templateUrl: absolute_path('/client/src/sensor/home.html'),
-        controller: 'sensor_controller'
-      });
+angular.module('main_routes', ['ngRoute'])
+  .config([
+    '$routeProvider',
+    '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'main/home.html',
+          controller: 'main_controller'
+        })
+        .when('/sensors', {
+          templateUrl: 'sensor/home.html',
+          controller: 'sensor_controller'
+        });
 
-    $locationProvider.html5Mode(true);
-  }]);
+      $locationProvider.html5Mode(true);
+    }
+  ]);
--- app/client/tests/main/routes_spec.js
+++ app/client/tests/main/routes_spec.js
@@ -1,14 +1,36 @@
-// describe('client.src.main.routes', function() {
-//   var route;
-//   var locationProvider;
-//
-//   beforeEach(module('main_routes'));
-//   beforeEach(inject(function($routeProvider, _$locationProvider_) {
-//     route = $routeProvider;
-//     locationProvider = _$locationProvider_;
-//   }));
-//
-//   it('should use the main controller on successful load of /', function() {
-//     expect(route.routes['/'].controller).toBe('main_controller');
-//   })
-// });
+describe('client.src.main.routes', function() {
+  var route;
+  var location;
+  var rootScope;
+
+  beforeEach(module('main_routes'));
+  beforeEach(inject(function(_$location_, _$route_, _$rootScope_) {
+    location = _$location_;
+    route = _$route_;
+    rootScope = _$rootScope_;
+  }));
+
+  it('should use the main controller on successful load of /', function() {
+    location.path('/');
+    rootScope.$digest();
+    expect(route.current.controller).toBe('main_controller');
+  });
+
+  it('should have main/home.html as the URL on successful load of /', function() {
+    location.path('/');
+    rootScope.$digest();
+    expect(route.current.loadedTemplateUrl).toBe('main/home.html');
+  });
+
+  it('should use the sensor controller on successful load of /sensors', function() {
+    location.path('/sensors');
+    rootScope.$digest();
+    expect(route.current.controller).toBe('sensor_controller');
+  });
+
+  it('should have sensor/home.html as the URL on successful load of /sensors', function() {
+    location.path('/sensors');
+    rootScope.$digest();
+    expect(route.current.loadedTemplateUrl).toBe('sensor/home.html');
+  });
+});
