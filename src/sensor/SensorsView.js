f937790b9a374a7092c914a7d79c1c2ebc60cad5
Gabriel Velasco <gabriel.velasco001@gmail.com>
2017-03-21 14:38:13 -0400

    Looking better

--- src/sensor/SensorsView.js
+++ src/sensor/SensorsView.js
@@ -4,6 +4,11 @@ const HashRouter = require('react-router-dom').HashRouter;
 const Route = require('react-router-dom').Route;
 const Redirect = require('react-router-dom').Redirect;
 const Button = require('react-bootstrap/lib/Button');
+const Panel = require('react-bootstrap/lib/Panel');
+const InputGroup = require('react-bootstrap/lib/InputGroup');
+const FormGroup = require('react-bootstrap/lib/FormGroup');
+const FormControl = require('react-bootstrap/lib/FormControl');
+const ControlLabel = require('react-bootstrap/lib/ControlLabel');
 
 const socket = io();
 
@@ -16,54 +21,35 @@ class Temp extends React.Component {
 
   render() {
     return (
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-lg-2'>
-            <div className='input-group'>
-              <span className='input-group-addon'>ID</span>
-              <input type='number' id='id' className='form-control' value='1234'/>
-            </div>
-          </div>
-
-          <div className='col-lg-2'>
-            <div className='input-group'>
-              <span className='input-group-addon'>Description</span>
-              <input type='text' id='description' className='form-control' value='A description'/>
-            </div>
-          </div>
-
-          <div className='col-lg-2'>
-            <div className='input-group'>
-              <span className='input-group-addon'>Latitude</span>
-              <input type='number' id='latitude' className='form-control' value='12.34'/>
-            </div>
-          </div>
-
-          <div className='col-lg-2'>
-            <div className='input-group'>
-              <span className='input-group-addon'>Longitude</span>
-              <input type='number' id='longitude' className='form-control' value='12.34'/>
-            </div>
-          </div>
-
-          <div className='col-lg-2'>
-            <div className='input-group'>
-              <span className='input-group-addon'>Temperature</span>
-              <input type='number' id='temperature' className='form-control' value='12'/>
-            </div>
-          </div>
-
-          <div className='col-lg-2'>
-            <div className='input-group'>
-              <span className='input-group-addon'>Relative Humidity</span>
-              <input type='number' id='relativeHumidity' className='form-control' value='34'/>
-            </div>
-          </div>
-
-          <div className='col-lg-1'>
-            <Button value="Add" bsStyle="primary" onClick={ this.handleCreate } />
-          </div>
-        </div>
+      <div>
+        <h1>Sensors</h1>
+        <Panel header='Create Sensor'>
+          <FormGroup>
+            <ControlLabel>ID</ControlLabel>
+            <FormControl type='number' placeholder='1234'/>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Description</ControlLabel>
+            <FormControl type='text' placeholder='1234'/>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Latitude</ControlLabel>
+            <FormControl type='number' placeholder='12.34'/>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Longitude</ControlLabel>
+            <FormControl type='number' placeholder='12.34'/>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Temperature</ControlLabel>
+            <FormControl type='number' placeholder='34'/>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Relative Humidity</ControlLabel>
+            <FormControl type='number' placeholder='12'/>
+          </FormGroup>
+          <Button bsStyle="primary" onClick={ this.handleCreate }>Create</Button>
+        </Panel>
       </div>
     )
   }
