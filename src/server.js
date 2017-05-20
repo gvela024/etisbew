3840e3b530b8f7020dd5edc6b2a983f881d20184
Gabriel Velasco <gabriel.velasco001@gmail.com>
2017-05-20 09:43:52 -0400

    Just had to use force

--- src/server.js
+++ src/server.js
@@ -22,20 +22,15 @@ module.exports = {
 
     const databaseUri = process.env.MONGODB_URI || localEnvironment;
     if (databaseUri !== localEnvironment) {
-      console.log('Securing site.');
-      console.log('Adding HTTP to HTTPS rerouting...');
-      app.use(secure.HTTPS({
-        trustProtoHeader: true
-      }));
-      console.log('done.');
-
-      console.log('HSTS...');
       const sixtyDaysInSeconds = 5184000
       app.use(helmet.hsts({
         maxAge: sixtyDaysInSeconds,
         force: true
       }));
-      console.log('done.');
+
+      app.use(secure.HTTPS({
+        trustProtoHeader: true
+      }));
     }
     app.use(express.static(path.join(__dirname, 'static'), {
       extensions: ['html', 'js', 'jsx']
